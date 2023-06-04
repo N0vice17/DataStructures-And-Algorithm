773355ecd59d7d0a83cef66bdb4f49727d96233c
Debojit <debojitganguly69@gmail.com>
2023-06-04 17:53:27 +0530

    deleting the dyanmic programming package

--- Trial.java
+++ Trial.java
@@ -3,17 +3,22 @@ import LinkedList.*;
 import Tries_and_Huffman_Coding.*;
 import java.util.*;
 public class Trial {
+	public static int helper(int[]arr,int []arr1,int sum,int ind,int w) {
+		if(ind>=arr.length) {
+			if(w==0) {
+				return sum;
+			}
+			return 0;
+		}
+		int ans=helper(arr,arr1,sum+arr[ind],ind+1,w-arr1[ind]);
+		int ans1=helper(arr,arr1,sum,ind+1,w);
+		return Math.max(ans, ans1);
+	}
     public static void main(String[]args){
     	Scanner input=new Scanner(System.in);
-    	int n=input.nextInt();
-    	int dp[]=new int[n+1];
-    	for(int i=1;i<=n;i++) {
-    		int min=Integer.MAX_VALUE;
-    		for(int j=1;j*j<=i;j++) {
-    			min=Math.min(min, dp[i-j*j]);
-    		}
-    		dp[i]=min+1;
-    	}
-    	System.out.println(dp[n]);
-    }    
+    	int arr[]= {100,200,300};
+    	int arr1[]= {20,25,30};
+    	int w=50;
+    	System.out.println(helper(arr,arr1,0,0,w));
+    }   
 }
