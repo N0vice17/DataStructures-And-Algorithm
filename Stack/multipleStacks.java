e448f831969e929c4c0aa56dc230a2de47872982
Debojit <debojitganguly69@gmail.com>
2023-06-12 12:31:30 +0530

    Adjusted the Packages of Stack

--- Stack/multipleStacks.java
+++ Stack/multipleStacks.java
@@ -1,90 +1,83 @@
+package Stack;
+
 import java.util.*;
-public class multipleStacks
-{
-    int topA,topB,stack[];
-    multipleStacks(int max)//parameterised constructor
+
+public class multipleStacks {
+    int topA, topB, stack[];
+
+    multipleStacks(int max)// parameterised constructor
     {
-        topA=-1;
-        topB=max;
-        stack=new int[max];
+        topA = -1;
+        topB = max;
+        stack = new int[max];
     }
-    void pushA(int n)//pushing into A
+
+    void pushA(int n)// pushing into A
     {
-        if(topA==topB-1)//checking for overflowing
+        if (topA == topB - 1)// checking for overflowing
         {
             System.out.println("Stack A Overflows");
-        }
-        else
-        {
+        } else {
             ++topA;
-            stack[topA]=n;
+            stack[topA] = n;
         }
     }
-    void pushB(int n)//pushing into stack B
+
+    void pushB(int n)// pushing into stack B
     {
-        if(topB-1==topA)//checking for overflowing
+        if (topB - 1 == topA)// checking for overflowing
         {
             System.out.println("Stack B Overflows");
-        }
-        else
-        {
+        } else {
             --topB;
-            stack[topB]=n;
+            stack[topB] = n;
         }
     }
-    int popA()//poping from A
+
+    int popA()// poping from A
     {
-        if(topA==-1)
-        {
+        if (topA == -1) {
             System.out.println("Stack A underflows");
             return (-999);
-        }
-        else
-        {
+        } else {
             int value;
-            value=stack[topA];
+            value = stack[topA];
             --topA;
             return (value);
         }
     }
-    int popB()//poping from B
+
+    int popB()// poping from B
     {
-        if(topB==stack.length)
-        {
+        if (topB == stack.length) {
             System.out.println("Stack B underflows");
             return (-999);
-        }
-        else
-        {
+        } else {
             int value;
-            value=stack[topB];
+            value = stack[topB];
             ++topB;
             return (value);
         }
     }
-    void displayA()
-    {
-        if(topA==-1)
-        {
+
+    void displayA() {
+        if (topA == -1) {
             System.out.println("Stack A is empty");
         }
         int i;
         System.out.println("Stack A");
-        for(i=topA;i>=0;i--)
-        {
+        for (i = topA; i >= 0; i--) {
             System.out.println(stack[i]);
         }
     }
-    void displayB()
-    {
-        if(topB==stack.length)
-        {
+
+    void displayB() {
+        if (topB == stack.length) {
             System.out.println("Stack B is empty");
         }
         int i;
         System.out.println("Stack B");
-        for(i=topB;i<stack.length;i++)
-        {
+        for (i = topB; i < stack.length; i++) {
             System.out.println(stack[i]);
         }
     }
