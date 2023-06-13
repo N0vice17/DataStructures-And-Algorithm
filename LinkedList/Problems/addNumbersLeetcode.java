e1a372aad5cea4c690af09ce08d040a2a9b5d0c6
Debojit <debojitganguly69@gmail.com>
2023-06-13 09:53:48 +0530

    Fixing some issues in Linkedlist Folder

--- LinkedList/Problems/addNumbersLeetcode.java
+++ LinkedList/Problems/addNumbersLeetcode.java
@@ -9,6 +9,15 @@
  * }
  */
 class Solution {
+    public class ListNode{
+        public int val;
+        public ListNode next;
+        public ListNode(){
+
+        }
+    public ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode temp1 = new ListNode(0);
         ListNode temp2 = temp1;
