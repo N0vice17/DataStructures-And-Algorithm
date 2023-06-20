e8042e4690b637b911a69d5c68166aa6f76f4ecd
Debojit <debojitganguly69@gmail.com>
2023-06-20 10:46:23 +0530

    Added Dijkstra Algorithm for graphs

--- Trial.java
+++ Trial.java
@@ -3,8 +3,8 @@ import LinkedList.*;
 import Tries_and_Huffman_Coding.*;
 import java.util.*;
 import Graphs.*;
-public class Trial {
-	public static void main(String[]args){
+public class Trial{
+    public static void main(String[]args){
         
     }
 }
