de69af83eab68f3e7cf86072877340b9ba80fc46
Jatin Lanje <j3lanje@gmail.com>
2023-10-02 10:01:26 +0530

    added 12 variations of nearest smaller/greater element

---
+++ Stack/JAVA/NearestGreaterElementOnLeft.java
@@ -0,0 +1,30 @@
+package Stack.JAVA;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class NearestGreaterElementOnLeft {
+    public static void main(String[] args) {
+        int arr[]=new int[]{4, 5, 2, 10, 8};
+        System.out.println(Arrays.toString(nearestGreaterElementOnLeft(arr)));
+    }
+
+    public static int[] nearestGreaterElementOnLeft(int[] arr) {
+        int[] res = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            if(stack.isEmpty()){ //res[i] = stack.isEmpty() ? -1 : arr[stack.peek()];
+                res[i]=-1;
+            }
+            else{
+                res[i]=stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        return res;
+    }
+}
