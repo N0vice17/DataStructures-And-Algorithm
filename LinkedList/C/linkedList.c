4407799f5a1b4665e61da674da1532c9d1133c4f
Letícia Aguiar <letsaguiar@yandex.com>
2023-10-03 17:57:20 -0300

    feat(LinkedList): create insert and delete at methods
    #50

--- LinkedList/C/linkedList.c
+++ LinkedList/C/linkedList.c
@@ -19,3 +19,26 @@ void	linked_list_delete(Node *p)
 	p->next = p->next->next;
 }
 
+void	linked_list_insert_at(Node *p, Node *q, int position)
+{
+	if (position == 0)
+		return (linked_list_insert(q, p));
+
+	int i = 0;
+	while (i < position && q)
+		q = q->next;
+	return (linked_list_insert(p, q));
+}
+
+void	linked_list_delete_at(Node *p, int position)
+{
+	if (position == 0)
+		return;
+
+	int i = 0;
+	while (i < position && p)
+		p = p->next;
+
+	if (p && p->next)
+		return (linked_list_delete(p));
+}
--- LinkedList/C/linkedList.h
+++ LinkedList/C/linkedList.h
@@ -9,6 +9,11 @@ typedef struct Node
 
 }	Node;
 
+/*
+ * Given an integer x and a node p,
+ * return the address of the first node
+ * where data is x or NULL if it doens't exists.
+ */
 Node	*linked_list_search(int x, Node *p);
 
 /*
@@ -16,10 +21,12 @@ Node	*linked_list_search(int x, Node *p);
  * inserts p between q and q.next
  */
 void	linked_list_insert(Node *p, Node *q);
+void	linked_list_insert_at(Node *p, Node *q, int position);
 
 /*
  * Given a node p, removes p.next from the list
  */
 void	linked_list_delete(Node *p);
+void	linked_list_delete_at(Node *p, int position);
 
 #endif
