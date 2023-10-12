eceeb47fee0c009e2a9ab9a1aff0ca7065d6746c
Nikunj Patel <patelnikunj2411@gmail.com>
2023-10-12 04:42:09 +0000

    Updated MergeSort.py check it #20

--- Sorting_Algorithm/PYTHON/MergeSort.py
+++ Sorting_Algorithm/PYTHON/MergeSort.py
@@ -1,28 +1,12 @@
-# Python program for implementation of MergeSort
-# Author :- NikunjPatel
-
 def mergeSort(arr):
 	if len(arr) > 1:
-
-		# Finding the mid of the array
-		mid = len(arr)//2
-
-		# Dividing the array elements
-		L = arr[:mid]
-
-		# Into 2 halves
-		R = arr[mid:]
-
-		# Sorting the first half
-		mergeSort(L)
-
-		# Sorting the second half
-		mergeSort(R)
-
+		mid = len(arr)//2   # Finding the mid of the array
+		L = arr[:mid] 		# Dividing the array elements
+		R = arr[mid:] 		# Into 2 halves
+		mergeSort(L) 		# Sorting the first half
+		mergeSort(R) 		# Sorting the second half
 		i = j = k = 0
-
-		# Copy data to temp arrays L[] and R[]
-		while i < len(L) and j < len(R):
+		while i < len(L) and j < len(R): 		# Copy data to temp arrays L[] and R[]
 			if L[i] <= R[j]:
 				arr[k] = L[i]
 				i += 1
@@ -30,25 +14,20 @@ def mergeSort(arr):
 				arr[k] = R[j]
 				j += 1
 			k += 1
-
-		# Checking if any element was left
-		while i < len(L):
+		while i < len(L):			# Checking if any element was left
 			arr[k] = L[i]
 			i += 1
 			k += 1
-
 		while j < len(R):
 			arr[k] = R[j]
 			j += 1
 			k += 1
-
 
 # Code to print the list
 def printList(arr):
 	for i in range(len(arr)):
 		print(arr[i], end=" ")
 	print()
-
 
 # Driver Code
 if __name__ == '__main__':
@@ -59,4 +38,11 @@ if __name__ == '__main__':
 	print("\nSorted array is ")
 	printList(arr)
 
+"""
+Output:
+Given array is
+12 11 13 5 6 7 
 
+Sorted array is 
+5 6 7 11 12 13 
+"""
